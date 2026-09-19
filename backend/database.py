a7098af5bce361cebc4e7196b8002d1e91fc4425
Yuval Verma <yuvalvermauv@gmail.com>
2026-09-19 18:33:58 +0530

    Support hosted MySQL connection

--- backend/database.py
+++ backend/database.py
@@ -9,6 +9,7 @@ load_dotenv()
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME", "campusos"),
